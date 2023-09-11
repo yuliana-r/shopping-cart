f59ea6e077230230fc17654e2640a67776ae91d4
yuli-r <iuliana.rotaru234@gmail.com>
2023-09-11 11:11:22 +0100

    Update Nav component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,23 @@
-function Nav() {
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+
+function Nav({ links }) {
   return(
     <nav>
-            Navigation bar goes here
+      {links.map((link) => {
+        <Link to={link.to} name={link.name}></Link>
+      })}
     </nav>
+  )
+}
+
+Nav.propTypes = {
+  links: PropTypes.arrayOf(
+    PropTypes.shape({
+      to: PropTypes.string,
+      name: PropTypes.string
+    })
   )
 }
 
