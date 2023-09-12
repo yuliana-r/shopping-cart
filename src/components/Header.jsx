ea951dd6b5335fbd32158cb5884c96a48b5f4072
yuli-r <iuliana.rotaru234@gmail.com>
2023-09-12 21:20:40 +0100

    Update header margins

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import Nav from "./Nav";
 
 function Header() {
   return(
-    <div className="flex flex-col items-center justify-center w-full border-b-2 border-solid shadow-lg select-none bg-black-100 border-silver-300">
-      <header className="flex flex-col items-center justify-center gap-2 p-3 text-center">
-        <i className="text-2xl text-yellow-300 fa-solid fa-shop"></i>
+    <div className="flex flex-col items-center justify-center w-full mt-4 border-b-2 border-solid shadow-lg select-none bg-silver-50 border-silver-300">
+      <header className="flex flex-col items-center justify-center gap-2 p-3 mb-4 text-center">
+        <i className="text-2xl text-yellow-400 fa-solid fa-shop"></i>
         <h1 className="text-4xl font-black text-navy-500 font-display">shop &apos;n&apos; roll</h1>
         <p className="text-4xl text-silver-800 font-motto">rollin&apos;, strollin&apos;, and sho-ppin&apos; away</p>
     </header>
