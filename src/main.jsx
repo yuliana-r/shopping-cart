11202132ce0a529104544f2fca76e1fd614401c7
yuli-r <iuliana.rotaru234@gmail.com>
2023-09-19 19:22:44 +0100

    Add redirects file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App';
-// import Router from './components/Router.jsx'
-// import Header from './components/Header.jsx'
-// import Footer from './components/Footer.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
