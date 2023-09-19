6c24db4b76881c20d503e3491585fc8eb85ebee0
yuli-r <iuliana.rotaru234@gmail.com>
2023-09-19 20:49:44 +0100

    Update top margins for main content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,8 +8,8 @@ export default function Home(props) {
   return(
     <>
       <Header cartItems={props.cartItems} />
-      <div className="mt-[240px] flex flex-col items-center justify-center w-full m-2 text-lg leading-7 text-justify select-none h-fit font-display bg-silver-50 text-black-950">
-        <div className="flex flex-row flex-wrap items-center w-[min(90vw,900px)] justify-center m-6 xl:flex-nowrap">
+      <div className="mt-[240px] xs:mt-[200px] flex flex-col items-center justify-center w-full m-2 text-lg leading-7 text-justify select-none h-fit font-display bg-silver-50 text-black-950">
+        <div className="flex flex-row flex-wrap items-center justify-center w-[min(90vw,900px)] m-6 xl:flex-nowrap">
           <p className="m-4">
                 At <span className="font-extrabold text-navy-500">shop &apos;n&apos; roll</span>, we&apos;ve 
                 crafted a unique online shopping experience that 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    screens: {
+      'xs': '415px',
+      ...defaultTheme.screens,
+    },
     colors: {
       'black': {
         '50': '#f6f6f6',
